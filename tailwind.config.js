5a292ee4e40df81097027df0ace95ba514cdbd81
jasenmichael <jasen@ready-builder.com>
2021-01-18 22:10:18 -0600

    chore: add images

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
     purge: [],
     darkMode: false, // or 'media' or 'class'
     theme: {
-      extend: {},
+      extend: {
+        padding: {
+          "18": '6rem'
+        },
+        colors: {
+          'corgiblue': '#3183C6'
+        },
+      },
     },
     variants: {
       extend: {},
