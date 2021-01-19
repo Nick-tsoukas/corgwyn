b04066e8064133861bee10dc6879a00de32d7b52
jasenmichael <jasen@ready-builder.com>
2021-01-18 22:09:55 -0600

    chore: create nav component

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,19 +1,45 @@
 <template>
-    <div>
-        <h1> {{ name }} </h1>
+  <div>
+    <img src="/logo.png" alt="Corgwin logo image" class="py-8 pl-18" />
+    <div
+      class="pl-18 py-4 text-sm text-white bg-corgiblue"
+      :class="scrollY >= 205 && 'navbar'"
+    >
+      <ul class="flex space-x-6 items-center justify-start">
+        <li class="">HOME</li>
+        <li class="">OUR CORGIS</li>
+        <li class="">ABOUT US</li>
+        <li class="">DONATE</li>
+        <li class="">SPONSOR A CORGI</li>
+        <li class="">CONTACT US</li>
+      </ul>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        data: function () {
-            return {
-                name: 'some mean man'
-            }
-        }
+export default {
+  data: () => {
+    return {
+      name: 'some mean man',
+      scrollY: 0,
     }
+  },
+  mounted() {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  methods: {
+    handleScroll() {
+      this.scrollY = window.scrollY
+    },
+  },
+}
 </script>
 
-<style lang="scss" scoped>
-
+<style>
+.navbar {
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
 </style>
