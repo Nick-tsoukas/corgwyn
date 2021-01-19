4f67bdebb59ec6cd7702c2d91667e025c7e35bbf
Nikolaos Tsoukas <nick.tsoukas101@gmail.com>
2021-01-18 18:31:00 -0600

    Setting up site

---
+++ components/Nav.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1> {{ name }} </h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return {
+                name: 'some mean man'
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/dog.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>this is the dog page</h1>
+    </div>
+</template>
+
