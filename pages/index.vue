b213caed68144e205972bffab0941d24cda68abc
jasenmichael <jasen@ready-builder.com>
2021-01-19 20:40:53 -0600

    chore: add notes

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- home head section   -->
     <h1 class="pl-12 pt-10 text-4xl font-sans">DONATE</h1>
     <div class="bg-gray-200 h-screen">
       <img src="/corgis-home.jpeg" alt="" />
@@ -10,6 +11,26 @@
         sponsorships, and an Amazon wishlist.
       </div>
     </div>
+    <!-- home head section   -->
+
+    <!-- learn more -->
+    <!-- ////// -->
+    <!-- learn more -->
+
+    <!-- SANCTUARY MEMBERS/HELP SUPPORT CRS cards -->
+    <div class="grid grid-cols-1 md:grid-cols-2"></div>
+    <!-- SANCTUARY MEMBERS/HELP SUPPORT CRS cards -->
+
+    <!-- SUPPORT CRS/HOW YOU CAN HELP  -->
+    <!-- NOTE: use flex not grid -->
+    <!-- donate -->
+    <!-- sponsor -->
+    <!-- support -->
+    <!-- SUPPORT CRS  -->
+
+    <!-- meet the corgis -->
+    <!-- carosel of current corgis -->
+    <!-- meet the corgis -->
   </div>
 </template>
 
