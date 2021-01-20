600ee852cea7eaa75b9551940e8a13567955aa5e
jasenmichael <jasen@ready-builder.com>
2021-01-19 20:51:21 -0600

    chore: move head to donate page

---
+++ pages/donate.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1 class="pl-12 pt-10 text-4xl font-sans">DONATE</h1>
+    <div class="bg-gray-200 h-screen">
+      <img src="/corgis-home.jpeg" alt="" />
+      <div>
+        Help Keep CRS Thriving! Corgwyn Rehabilitation Sacntuary is now
+        officially a 501(c)(3)! This means all donations are tax deductible. We
+        offer several forms of donation, including cash donations, corgi
+        sponsorships, and an Amazon wishlist.
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
