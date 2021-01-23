10e05c5cb77612177d2d7cec066bdd8cee36a92f
jasenmichael <jasen@ready-builder.com>
2021-01-22 22:06:00 -0600

    chore: fix about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,143 +1,143 @@
 <template>
   <div>
-    <div class="pb-10 text-center lg:text-left">
-      <h1 class="text-4xl pt-10">ABOUT US</h1>
-      <h2>CORGWYN REHABILITATION SANCTUARY</h2>
-    </div>
+    <h1 class="text-4xl ml-6 text-gray-900 px-2 py-2">ABOUT US</h1>
+    <div class="bg-gray-100">
+      <h3
+        class="px-4 py-2 mb-10 text-3xl font-black text-corgiblue opacity-80 pt-8"
+      >
+        CORGWYN REHABILITATION SANCTUARY
+      </h3>
+      <div class="mx-14 text-gray-500">
+        <p>
+          <img
+            src="/about.jpeg"
+            align="left"
+            alt="image of Bret"
+            class="pr-3 pb-3"
+          />
+          <span class="font-bold">OUR DREAM: </span>A self sustaining Corgwyn
+          Rehabilitation Sanctuary (CRS)
+          <br />
+          <span class="font-bold"> OUR NEED: </span>YOU!
+        </p>
+        <br />
 
-    <div class="my_color">
-      <div class="container mx-auto h-auto mb-10">
-        <div class="text-center lg:text-left">
-          <h1 class="text-4xl pt-10 blue_text pb-5">ABOUT</h1>
-          <h2 class="blue_text">CORGWYN REHABILITATION SANCTUARY</h2>
-        </div>
-        <section class="pt-10">
-          <div class="lg:flex">
-            <img
-              class="h-96 w-screen object-contain lg:object-bottom"
-              src="../static/about_owner.jpg"
-              alt=""
-            />
-            <div class="p-10 lg:flex-grow">
-              <p class="pb-10 text-lg">
-                <span class="text-gray-600 font-bold">OUR DREAM:</span> A self
-                sustaining Corgwyn Rehabilitation Sanctuary (CRS)
-              </p>
-              <p class="pb-10 text-lg">
-                <span class="text-gray-600 font-bold">OUR NEED:</span> YOU!
-              </p>
-              <p class="pb-10 text-lg">
-                Hello, my name is Brett Butler and for 30 years I have been
-                rescuing and rehabilitating dogs for placement with new
-                families. The last 8 years, corgis have been my sole focus.
-                Corgis are such amazing, fun, smart, cute, energetic &
-                challenging kids; they just won my heart. We corgi lovers know
-                the addiction is real.
-              </p>
-            </div>
-          </div>
+        <p>
+          Hello, my name is Brett Butler and for 30 years I have been rescuing
+          and rehabilitating dogs for placement with new families. The last 8
+          years, corgis have been my sole focus. Corgis are such amazing, fun,
+          smart, cute, energetic & challenging kids; they just won my heart. We
+          corgi lovers know the addiction is real.
+        </p>
+        <br />
 
-          <div class="container mx-auto pl-10 pr-10">
-            <p class="text-lg pb-8 pt-12">
-              Over time my home has emerged as an unofficial corgi sanctuary and
-              rehab, whereby corgi rescuers nationwide bring corgis to me. I am
-              very grateful to have their trust, and the ability to provide this
-              service. My pack and I have built a solid reputation as we focus
-              on helping corgis heal their spirits, resolve behavior issues, and
-              repair trust. It’s a team effort of rescuers, my personal 5
-              corgis, and the numbers of corgis who rotate in and out of my
-              home. Consequently, I now have some corgis who will never leave
-              due to issues. They function well in the structure of our pack,
-              but we are unsure we can ever rehome them.
-            </p>
-            <p class="text-lg">
-              Corgwyn Rehab is a Sanctuary for the hard luck cases, the last
-              resort cases, the bad behavior cases. We have taken in many that
-              were scheduled to be Euthanized but were sent here instead and now
-              function as a vital part of my pack. Our Goal is the help these
-              corgwyn get to a balanced state of being so they can function as a
-              part of the pack. If, in time working with a dog, we see that they
-              have the ability to be rehomed then we will see to that also. Our
-              main function is not to rehome, it is to rehabilitate these
-              special furkids. Some will be rehabilitated to that point but
-              others will always be safe as a part of our pack. They are damaged
-              too severely. I have learned that most of the damage is caused by
-              forces outside of their control and with my help they can be
-              turned around and function well in our pack.
-            </p>
-            <p class="text-lg pt-8">
-              This is an expensive endeavor. I am pursuing work to generate
-              income for my base expenses; with additional funds needed to cover
-              my corgi expenses. Last year my vet & med bills exceeded $8,000;
-              and there was another $3,000 on food, toys & chew bones. I realize
-              that in order for me to provide sanctuary for these corgis, I need
-              investors help.
-            </p>
-            My short term goal is to continue to operate successfully in my
-            current space (fenced yard, no kids). My long term goal is to find a
-            property even more suited for a larger more private sanctuary. As of
-            9/2018 we are close to reaching this goal.
-            <p class="text-lg pt-8">
-              I have three primary areas of expense: Veterinary medicine, food
-              and consumables, and supplies (to include the wear and tear on my
-              home (carpet, doors, drywall, trim; which I can do the labor
-              myself). My desire is to create sponsorships or donation streams
-              that can fund these accounts (all overseen by others not just me).
-              Until we get 501c3 status, donors are welcome to donate directly
-              to our vet, and trust that this is not in any way funding my
-              personal life.
-            </p>
-            <p class="text-lg pt-8">
-              I am working with other experienced rescuers for advice and
-              direction. My hope is that by assembling a core team of advisors
-              and investors we can create a corgi sanctuary and serve the needs
-              of corgis from our central location. I decided to take a risk and
-              humbly ask for your support, because I know the incredible power
-              of passionate people working in groups as I was part of the
-              initial creation of the Corgi Coalition Transport Team. CCTT
-              showed me that those of us with a will, find a way. And I know
-              that if I reach out many great people will enable miracles to
-              happen.
-            </p>
-            <p class="text-lg pt-8">
-              Our CRS administrators, at present, consists of myself, Brett
-              Butler, and our secretary, LeAnne Foust. Due to my job, I may not
-              be able to answer you as quickly as LeAnne can. Please know that
-              we are in daily contact and she will always make sure I am
-              informed. She is running our paperwork side of this endeavor and I
-              am working with the corgwyn.
-            </p>
-            Our CRS administrators, at present, consists of myself, Brett
-            Butler, and our secretary, LeAnne Foust. Due to my job, I may not be
-            able to answer you as quickly as LeAnne can. Please know that we are
-            in daily contact and she will always make sure I am informed. She is
-            running our paperwork side of this endeavor and I am working with
-            the corgwyn.
-            <p class="text-lg pt-8">
-              If you choose to invest, I will work with my team to ensure
-              updates so that you will know the difference you make. You can
-              donate in small monthly amounts, single donation, or bi-annually;
-              all directly to the funds or vets. As many of you know, running
-              fundraisers is extremely time consuming & challenging. And sadly,
-              my finances just do not allow me the luxury cushion I once had to
-              easily provide for the corgis. Your participation will enable us
-              to build a sustainable future to help corgis. Thank you seems too
-              small a phrase to convey my gratitude; but know I most appreciably
-              thank you!
-            </p>
-          </div>
-        </section>
-        <section class="pb-10 pl-10 pr-10">
-          <h1 class="text-2xl font-bold pt-10">
+        <p>
+          Over time my home has emerged as an unofficial corgi sanctuary and
+          rehab, whereby corgi rescuers nationwide bring corgis to me. I am very
+          grateful to have their trust, and the ability to provide this service.
+          My pack and I have built a solid reputation as we focus on helping
+          corgis heal their spirits, resolve behavior issues, and repair trust.
+          It’s a team effort of rescuers, my personal 5 corgis, and the numbers
+          of corgis who rotate in and out of my home. Consequently, I now have
+          some corgis who will never leave due to issues. They function well in
+          the structure of our pack, but we are unsure we can ever rehome them.
+        </p>
+        <br />
+
+        <p>
+          Corgwyn Rehab is a Sanctuary for the hard luck cases, the last resort
+          cases, the bad behavior cases. We have taken in many that were
+          scheduled to be Euthanized but were sent here instead and now function
+          as a vital part of my pack. Our Goal is the help these corgwyn get to
+          a balanced state of being so they can function as a part of the pack.
+          If, in time working with a dog, we see that they have the ability to
+          be rehomed then we will see to that also.
+        </p>
+        <br />
+
+        <p>
+          Our main function is not to rehome, it is to rehabilitate these
+          special furkids. Some will be rehabilitated to that point but others
+          will always be safe as a part of our pack. They are damaged too
+          severely. I have learned that most of the damage is caused by forces
+          outside of their control and with my help they can be turned around
+          and function well in our pack.
+        </p>
+        <br />
+
+        <p>
+          This is an expensive endeavor. I am pursuing work to generate income
+          for my base expenses; with additional funds needed to cover my corgi
+          expenses. Last year my vet & med bills exceeded $8,000; and there was
+          another $3,000 on food, toys & chew bones. I realize that in order for
+          me to provide sanctuary for these corgis, I need investors help.
+        </p>
+        <br />
+
+        <p>
+          My short term goal is to continue to operate successfully in my
+          current space (fenced yard, no kids). My long term goal is to find a
+          property even more suited for a larger more private sanctuary. As of
+          9/2018 we are close to reaching this goal.
+        </p>
+        <br />
+
+        <p>
+          I have three primary areas of expense: Veterinary medicine, food and
+          consumables, and supplies (to include the wear and tear on my home
+          (carpet, doors, drywall, trim; which I can do the labor myself). My
+          desire is to create sponsorships or donation streams that can fund
+          these accounts (all overseen by others not just me). Until we get
+          501c3 status, donors are welcome to donate directly to our vet, and
+          trust that this is not in any way funding my personal life.
+        </p>
+        <br />
+
+        <p>
+          I am working with other experienced rescuers for advice and direction.
+          My hope is that by assembling a core team of advisors and investors we
+          can create a corgi sanctuary and serve the needs of corgis from our
+          central location. I decided to take a risk and humbly ask for your
+          support, because I know the incredible power of passionate people
+          working in groups as I was part of the initial creation of the Corgi
+          Coalition Transport Team. CCTT showed me that those of us with a will,
+          find a way. And I know that if I reach out many great people will
+          enable miracles to happen.
+        </p>
+        <br />
+
+        <p>
+          Our CRS administrators, at present, consists of myself, Brett Butler,
+          and our secretary, LeAnne Foust. Due to my job, I may not be able to
+          answer you as quickly as LeAnne can. Please know that we are in daily
+          contact and she will always make sure I am informed. She is running
+          our paperwork side of this endeavor and I am working with the corgwyn.
+        </p>
+        <br />
+
+        <p>
+          If you choose to invest, I will work with my team to ensure updates so
+          that you will know the difference you make. You can donate in small
+          monthly amounts, single donation, or bi-annually; all directly to the
+          funds or vets. As many of you know, running fundraisers is extremely
+          time consuming & challenging. And sadly, my finances just do not allow
+          me the luxury cushion I once had to easily provide for the corgis.
+          Your participation will enable us to build a sustainable future to
+          help corgis. Thank you seems too small a phrase to convey my
+          gratitude; but know I most appreciably thank you!
+        </p>
+        <br />
+
+        <div class="flex items-center pb-20">
+          <h2 class="text-2xl font-black text-corgiblue mr-4">
             READY TO HELP? EVERY DONATION COUNTS!
-          </h1>
+          </h2>
           <nuxt-link to="/donate">
-            <button class="bg-black text-white p-4 mt-4">DONATE NOW</button>
+            <button class="bg-corgiblue rounded py-2 px-3 text-white">
+              DONATE NOW
+            </button>
           </nuxt-link>
-          <nuxt-link to="/about2">
-            <button class="bg-black text-white p-4 mt-4">DONATE NOW</button>
-          </nuxt-link>
-        </section>
+        </div>
       </div>
     </div>
   </div>
@@ -146,12 +146,3 @@
 <script>
 export default {}
 </script>
-<style scoped>
-.my_color {
-  background: #efeef1;
-}
-
-.blue_text {
-  color: #6ec1e3;
-}
-</style>
