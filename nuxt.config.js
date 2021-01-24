0f50e480e72b61c6a023794cfee4ae30e0b8cb79
jasenmichael <jasen@ready-builder.com>
2021-01-23 18:29:54 -0600

    feat: implement pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,5 @@
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'CORGWYN REHABILITATION SANCTUARY',
     htmlAttrs: {
@@ -13,33 +10,20 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'A self sustaining Corgwyn Rehabilitation Sanctuary (CRS)' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/png', href: '/icon-32x32.png', sizes:'32x32' },
-      { rel: 'icon', type: 'image/png', href: '/icon-192x192.png', sizes:'192x192' }
-    ]
+    // link: [
+    //   { rel: 'icon', type: 'image/png', href: '/icon-32x32.png', sizes:'32x32' },
+    //   { rel: 'icon', type: 'image/png', href: '/icon-192x192.png', sizes:'192x192' }
+    // ]
   },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  css: [],
   plugins: [
     { src: './plugins/smooth-scroll', mode: 'client' }
   ],
-
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
     '@nuxtjs/pwa',
   ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     '@nuxt/content',
     '@nuxt/image'
@@ -47,8 +31,6 @@ export default {
   image: {
     sizes: [200, 350, 600]
   },
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  pwa: {},
+  build: {}
 }
